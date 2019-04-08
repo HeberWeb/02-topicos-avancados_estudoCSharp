7767b67d6c509c293de70ff972aec6f5569293f0
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-08 16:28:20 -0300

    nullable

---
+++ 04_Nullable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Struct - sendo por valor, contendo um limite minumo e maximo no caso do int, e não recebe valor null
+            //int i = null;
+
+            //Class - por referencia podendo receber valores null
+            object o = null;
+
+            Nullable<int> Idade = null;
+            int? Idade2 = null;
+        }
+    }
+}

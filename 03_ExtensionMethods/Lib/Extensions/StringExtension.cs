6eba8de3da88385591bac87cf3589cc93deba966
Heber Pietrafeza <hpietrafeza@autodoc.com.br>
2019-04-08 16:07:04 -0300

    extension methods

---
+++ 03_ExtensionMethods/Lib/Extensions/StringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public static class StringExtension
+    {
+        public static string FirstToUpper(this String str)
+        {
+            string first = str.Substring(0, 1);
+            string second = str.Substring(1);
+            return first.ToUpper() + second;
+        }
+    }
+}
